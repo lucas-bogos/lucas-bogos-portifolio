a27070a630e4871b577a42ed5869b77714db1b9f
Lucas Bogos <lucas@pop-os.localdomain>
2022-03-11 22:15:14 -0300

    altera o gif para uma imagem de agradecimento

--- src/pages/agradecimento.jsx
+++ src/pages/agradecimento.jsx
@@ -1,9 +1,15 @@
 import styles from "../styles/Agradecimento.module.css";
+import Image from "next/image";
 
 export default function agradecimento() {
     return <div className={styles.agradecimento}>
         <h1>Obrigado pelo contato!</h1>
-        <div className="tenor-gif-embed" data-postid="23282582" data-share-method="host" data-aspect-ratio="1.77778" data-width="100%"><a href="https://tenor.com/view/happy-developer-happy-software-developer-happy-dev-gif-23282582">Happy Developer Happy Software Developer GIF</a>from <a href="https://tenor.com/search/happy+developer-gifs">Happy Developer GIFs</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
         <p>Devido à várias requisições diariamente, a resposta de seu contato <strong>poderá levar até 72h</strong>.</p>
+        <Image
+            src="/images/thank-you.jpg"
+            width="300"
+            height="300"
+            alt="lucas-bogos-agradece-pelo-contato"
+        />
     </div>
 }
