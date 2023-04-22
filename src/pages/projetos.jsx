82ebe710a472e5bbe761d98a534ba13d21656eff
Lucas Bogos <bogoslucas1@gmail.com>
2023-04-22 15:20:36 -0300

    coloca uma revalidação de 6h nos projetos

--- src/pages/projetos.jsx
+++ src/pages/projetos.jsx
@@ -1,50 +1,46 @@
 import styles from "../styles/Projetos.module.css";
+import { getYearUpdated } from "../utils";
 
 export async function getStaticProps() {
-  const response = await fetch("https://api.github.com/users/lucas-bogos/repos");
+  const BASE_URL = "https://api.github.com/users/lucas-bogos";
+  const SIX_HOURS = 60 * 60 * 6;
+
+  const response = await fetch(`${BASE_URL}/repos`);
   const data = await response.json();
 
   return {
-    props: { 
+    props: {
       repositories: data
-     }
+    },
+    revalidate: SIX_HOURS
   }
 }
+
 export default function projetos({ repositories }) {
-  const getYearUpdated = (date) => {
-    const yearUpdated = parseInt(date.slice(0, 4));
-    return yearUpdated;
-  }
-
-  const getMonthUpdated = (date) => {
-    const monthUpdated = parseInt(date.slice(6, 7));
-    return monthUpdated;
-  }
-
   return <>
     <h1 className={styles.title}>Projetos recentes</h1>
     <div className={styles.card_container}>
-    {repositories.map((repo, index) => (
-    <div>{
-      repo.name !== "lucas-bogos-portifolio" 
-      && repo.name !== "lucas-bogos" 
-      && repo.name !== "email-marketing" 
-      && getYearUpdated(repo.created_at) >= 2021 &&
-      <div className={styles.card_repo} key={index}>
-        <h3 className={styles.title}>{repo.name.replace(/-/g, " ")}</h3>
-        <p>Descrição: {repo.description}</p>
-        <div className={`${styles.stack} ${styles[repo.language]}`}>
-          <p>{repo.language}</p>
-        </div>
-          <div className={styles.cta_container}>
-            <a href={`https://github.com/${repo.full_name}`} target="_blank">
-              <span className={styles.cta}>
-                Saiba mais
-              </span>
-            </a>
+      {repositories.map((repo, index) => (
+        <div>{
+          repo.name !== "lucas-bogos-portifolio"
+          && repo.name !== "lucas-bogos"
+          && repo.name !== "email-marketing"
+          && getYearUpdated(repo.created_at) >= 2021 &&
+          <div className={styles.card_repo} key={index}>
+            <h3 className={styles.title}>{repo.name.replace(/-/g, " ")}</h3>
+            <p>Descrição: {repo.description}</p>
+            <div className={`${styles.stack} ${styles[repo.language]}`}>
+              <p>{repo.language}</p>
+            </div>
+            <div className={styles.cta_container}>
+              <a href={`https://github.com/${repo.full_name}`} target="_blank">
+                <span className={styles.cta}>
+                  Saiba mais
+                </span>
+              </a>
+            </div>
           </div>
-      </div>
-    }</div>
-  ))}</div>
+        }</div>
+      ))}</div>
   </>
 }
